4873ebc6216b05b6c9f196abc5966a038db6c9a0
newbission <155050120+newbission@users.noreply.github.com>
2024-09-09 14:57:22 +0900

    Feat: api 다이어리까지 추가(일부 안됨 이유 모름) (#2)

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 # app/main.py
 from fastapi import FastAPI
 
+from api.v1 import V1
+
+
 app = FastAPI()
+app.include_router(V1)
 
 
 @app.get("/")
 def read_root():
     return {"DDRAWRY": "This is ddrawry's API server"}
+
