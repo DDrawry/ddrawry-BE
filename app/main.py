b9db2cade4d36ad975241cf4f4b52b6d45522bff
Kim <58137602+sub-blind@users.noreply.github.com>
2024-09-15 20:33:34 +0900

    Feat: Database setting complete (#9) (#10)

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
-# from .database import engine, Base
-# Base.metadata.create_all(bind=engine) # DB연결 아직 안할거니까
+from .database import engine, Base
+from .models import *  # 모델을 임포트하여 테이블을 생성하도록 함
+
+# DB 테이블을 생성
+Base.metadata.create_all(bind=engine) 
 
 from .api.v1 import V1
 
 app = FastAPI()
 app.include_router(V1)
 
-
 @app.get("/")
 def read_root():
     return {"DDRAWRY": "This is ddrawry's API server"}
-
-
