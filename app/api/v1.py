d93512c85524b50a8f790452a8e2b8db117e1c64
GrapeKang <kang@Podo.local>
2024-09-24 23:51:01 +0900

    Feat: 액세스 토큰, 리프레쉬 토큰 기능 추가

--- app/api/v1.py
+++ app/api/v1.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter
 from .users import router as users_router
 from .diaries import router as diaries_router
 from .auth import router as auth_router
+from .auth import is_valid_token
+from fastapi import APIRouter, HTTPException, Request
 
 V1 = APIRouter(prefix="/api/v1")
 
@@ -17,5 +19,10 @@ V1.include_router(auth_router)
 
 
 @V1.get("/", tags=["v1"])
-async def start_v1():
+async def start_v1(request: Request):
+    access_token = request.cookies.get("access_token")
+    
+    if not access_token or not is_valid_token(access_token):
+        raise HTTPException(status_code=401, detail="Invalid or missing access token")
+
     return {"msg": "DDrawry's API version 1"}
