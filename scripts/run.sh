7236ec29e0f44a45c51c80ed85065d55455b8bf6
GrapeKang <kang@Podo.local>
2024-10-17 18:14:05 +0900

    도커 파일 수정

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,8 +6,5 @@ echo "Current Directory: $(pwd)"
 echo "Contents:"
 ls -la
 
-# 데이터베이스 마이그레이션 (Alembic을 사용하는 경우)
-alembic upgrade head
-
 # FastAPI 애플리케이션을 Gunicorn + Uvicorn worker로 실행
 exec gunicorn -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 app.main:app
