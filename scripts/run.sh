9d09d2b2ea0b3afd288b1e1423a43cf11044782f
GrapeKang <kang@Podo.local>
2024-11-17 17:54:46 +0900

    Feat: run.sh 수정

--- scripts/run.sh
+++ scripts/run.sh
@@ -15,4 +15,5 @@ export PYTHONPATH=/ddrawry
 
 # FastAPI 애플리케이션을 Gunicorn + Uvicorn worker로 실행
 echo "Starting FastAPI application..."
-exec gunicorn -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 app.main:app
+# 로그 레벨을 debug로 설정
+exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --timeout 120 --log-level debug app.main:app
